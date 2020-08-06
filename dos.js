d8d8ca6c7e5345045730c79a9099ef6732e52dc9
noelialeguizamon <68036315+noelialeguizamon@users.noreply.github.com>
2020-08-06 18:22:46 -0300

    nuevo

--- dos.js
+++ dos.js
@@ -10,7 +10,53 @@ Al finalizar, se debe informar:
 Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas y que el precio no puede ser menor a 0 ni mayor a 1000.
 */
 
-function mostrar()
-{
-	alert("Hola Mundo");
+function mostrar() {
+	let precioActual;
+	let nombreActual;
+	let categoriaActual;
+	let precioArtMasCaro = 0;
+	let precioLacteoMasCaro = 0;
+	let cantbebidas = 0;
+	let nombreArtMasCaro = " ";
+	let nombreLacteoMasCaro = " ";
+
+
+	do {
+		precioActual = parseInt(prompt("ingrese precio del articulo: "));
+		nombreActual = prompt("nombre del articulo: ");
+		categoriaActual = prompt("ingrese una categoria: ");
+		if (categoriaActual == "bebidas" || categoriaActual == "almacen" || categoriaActual == "lacteos" || categoriaActual == "limpieza") {
+			if (precioActual > 0 && precioActual < 1000) {
+
+				if (categoriaActual == "bebidas") {
+
+					cantbebidas = cantbebidas + 1;
+				}
+				if (precioActual > precioArtMasCaro) {
+
+					precioArtMasCaro = precioActual;
+					nombreArtMasCaro = nombreActual;
+
+				}
+				if (categoriaActual == "lacteos" && precioActual > precioLacteoMasCaro) {
+
+					precioLacteoMasCaro = precioActual;
+					nombreLacteoMasCaro = nombreActual;
+				}
+			}
+			else {
+				alert("No corresponde, vuelva a ingresar los datos");
+			}
+
+		}
+		else{
+			alert("categoria incorrecta, elija entre lacteos, almacen, bebidas o limpieza");
+		}
+	}
+	while (categoriaActual != "salir");
+
+	alert("el articulo mas caro: " + nombreArtMasCaro + "\n lacteo mas caro: " + nombreLacteoMasCaro + "\n la cantidad de bebidas son: " + cantbebidas);
+
+
+
 }
